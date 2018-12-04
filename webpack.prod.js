0aa1dab81cdc1f47817e9982bf48890e1b5695d4
leixu <leixu@quantongfu.com>
2018-12-04 17:17:10 +0800

    source map

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,14 @@
-// 去除无用的引入
+// 去除无用的引入，压缩代码
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
+    devtool: 'source-map',
     plugins: [
-        new UglifyJSPlugin()
+        // new UglifyJSPlugin()
+        new UglifyJSPlugin({
+            sourceMap: true
+        })
     ]
 });
