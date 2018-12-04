52a5919fba144f82cb222000ffb1d47d9f0ba162
leixu <leixu@quantongfu.com>
2018-12-04 11:37:42 +0800

    模块热替换

--- README.md
+++ README.md
@@ -6,3 +6,10 @@
 #### 学习webpack的第三天
 
 #### 可以入门以后的路就好走了些
+
+##### webpack 模块热替换
+
+通过 Node.js API,当使用 webpack dev server 和 Node.js API 时，不要将 dev server 选项放在 webpack 配置对象(webpack config object)中。而是，在创建选项时，将其作为第二个参数传递。
+例如：
+new WebpackDevServer(compiler, options)
+
--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
-import _ from "lodash"; 
-
-import printMe from "./print.js";
+import _ from 'lodash';
+import printMe from './print.js';
+import './styles.css';
 
 function component() {
-
     var element = document.createElement('div');
+    var btn = document.createElement('button');
 
-    var btn = document.createElement("button");
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-    element.innerHTML = _.join(['Hello', ' webpack'], '');
-
-    btn.innerHTML = "点击按钮执行方法console!";
-
-    btn.onclick = printMe;
+    btn.innerHTML = '点击按钮触发时间console!';
+    btn.onclick = printMe; // onclick 事件绑定原始的 printMe 函数上
 
     element.appendChild(btn);
 
     return element;
 }
 
-document.body.appendChild(component());
+let element = component(); // 当 print.js 改变导致页面重新渲染时，重新获取渲染的元素
+
+document.body.appendChild(element);
+
+if (module.hot) {
+    module.hot.accept('./print.js', function () {
+        console.log('接受更新的printMe模块！!');
+        printMe();
+        document.body.removeChild(element);
+        element = component(); // 重新渲染页面后，component 更新 click 事件处理
+        document.body.appendChild(element);
+    })
+}
